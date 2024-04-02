2729e9d01ef9bb593f20a636215227a1d42f28e8
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-04-02 22:49:51 +0200

    task1 fix

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,14 +7,13 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('Copyright - Holberton School');
 
-let count = 0;
-let countElement = $('#count');
-const button = $('button')[0];
 function updateCounter() {
-  count += 1;
-  countElement.text(`${count} clicks on the button`);
+  let count = $('#count').html() || 0;
+  $('button').on('click', () => {
+    count += 1;
+    $('#count').html(`${count} clicks on the button`);
+  });
 }
 
 _.debounce(updateCounter, 4000);
-button.on('click', updateCounter);
 updateCounter();
