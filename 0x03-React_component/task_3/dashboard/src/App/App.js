e96adca2b57f36ee50da540ad5b1a3a27b5e204b
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-04-09 17:02:33 +0200

    task3p3

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -7,6 +7,8 @@ import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList.js';
 import { getLatestNotification } from '../utils/utils.js';
 import { Component } from 'react';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom.js';
+import BodySection from '../BodySection/BodySection.js';
 
 const courses = [
   { id: 1, name: 'ES6', credit: 60 },
@@ -45,8 +47,31 @@ class App extends Component {
         <Notifications listNotifications={notifications} />
         <div className="App">
           <Header />
-          {isLoggedIn && <CourseList listCourses={courses} />}
-          {!isLoggedIn && <Login />}
+          {isLoggedIn && (
+            <>
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={courses} />
+              </BodySectionWithMarginBottom>
+              <BodySection title="News from the School">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </BodySection>
+            </>
+          )}
+          {!isLoggedIn && (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+
           <Footer />
         </div>
       </>
